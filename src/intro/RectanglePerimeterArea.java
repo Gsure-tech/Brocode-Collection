9a4432da9ac416184e61559a3a5be3b168831a7c
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-10-17 16:27:06 +0100

    implemented Perimeter of a rectangle returning void

--- src/intro/RectanglePerimeterArea.java
+++ src/intro/RectanglePerimeterArea.java
@@ -12,4 +12,14 @@ public class RectanglePerimeterArea {
         double area = length * width;
         System.out.printf("Area of rectangle is: %.2f", area);
     }
+    public void perimeterOfRectangle() {
+        double length = 35;
+        double width = 20;
+        double perimeter =  2 * (length * width);
+        System.out.printf("Perimeter of rectangle is: %.2f", perimeter);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
