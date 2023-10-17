757e81cd7598a09dc0b0adc9d1b4ea75778bf052
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-10-17 15:42:03 +0100

    implemented area of a rectangle returning void

--- src/intro/RectanglePerimeterArea.java
+++ src/intro/RectanglePerimeterArea.java
@@ -2,7 +2,14 @@ package intro;
 
 public class RectanglePerimeterArea {
 
-    public double calculateArea(int width, int length){
+    public double calculateArea(int width, int length) {
         return width * length;
+    }
+
+    public void areaOfRectangle() {
+        double length = 25;
+        double width = 30;
+        double area = length * width;
+        System.out.printf("Area of rectangle is: %.2f", area);
     }
 }
